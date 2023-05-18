a9a45140534097500e76cdb4ecfcbd1b2982cbf3
Kevin Tan <33914892+kevtan@users.noreply.github.com>
2023-05-17 22:22:04 -0700

    ✨ Implement register renaming

--- core/renaming_map.sv
+++ core/renaming_map.sv
@@ -34,6 +34,56 @@ module renaming_map import ariane_pkg::*; #(
     logic [PHYS_REG_WIDTH-1:0] rd;
 
     // TODO: ADD STRUCTURES TO EXECUTE REGISTER RENAMING
+    
+    // There'll be one entry per architectural register.
+    typedef struct packed {
+        logic                      valid;   // Valid bit
+        logic [PHYS_REG_WIDTH-1:0] pr;      // Physical register
+    } rename_table_entry_t;
+
+    // Maps architectural registers to physical registers.
+    rename_table_entry_t rename_table[ARCH_NUM_REGS];
+
+    // Initialize the rename table to only map ar0 to pr0.
+    initial begin
+        rename_table[0].valid = 1;
+        rename_table[0].pr = 0;
+        for (int i = 1; i < ARCH_NUM_REGS; i++) begin
+            rename_table[i].valid = 0;
+        end
+    end
+
+    // Maps physical registers to whether or not they're free.
+    //  0: free
+    //  1: allocated
+    // Initially, pr0 is the only physical register allocated.
+    logic [PHYS_NUM_REGS-1:0] pr_free_map = '1;
+
+    typedef struct packed {
+        logic                      valid;   // Valid bit
+        logic [PHYS_REG_WIDTH-1:0] pr;      // Physical register
+    } deallocation_map_entry_t;
+
+    // The deallocation map tells us: when a physical register is the destination
+    // of the committing instruction (i.e., it's currently being written with the
+    // result of a compution), whether another physical register should now be
+    // deallocated.
+    //
+    // When the destination register of an instruction already has a mapping in
+    // the rename map, we *can't* deallocate its current associated physical
+    // register during instruction decode (ID) because there might still be
+    // instructions in execution that reference the register. However, when the
+    // instruction *commits*, because commits happen in program order, we know
+    // for sure that no instructions in execution reference the old register
+    // because all subsequent instructions use the new physical register mapping.
+    deallocation_map_entry_t deallocation_map[PHYS_NUM_REGS];
+
+    // Initialize the deallocation map.
+    initial begin
+        for (int i = 0; i < PHYS_NUM_REGS; i++) begin
+            deallocation_map[i].valid = 0;
+        end
+    end
 
     // Positive clock edge used for renaming new instructions
     always @(posedge clk_i, negedge rst_ni) begin
@@ -41,6 +91,21 @@ module renaming_map import ariane_pkg::*; #(
         if (~rst_ni) begin
 
             // TODO: ADD LOGIC TO RESET RENAMING STATE
+            
+            // Clear out the rename table.
+            for (int i = 1; i < ARCH_NUM_REGS; i++) begin
+                rename_table[i].valid = 0;
+            end
+
+            // Mark all physical registers (except pr0) as free again.
+            for (int i = 1; i < PHYS_NUM_REGS; i++) begin
+                pr_free_map[i] = 0;
+            end
+
+            // Clearing the deallocation map.
+            for (int i = 0; i < PHYS_NUM_REGS; i++) begin
+                deallocation_map[i].valid = 0;
+            end
     
         // New incoming valid instruction to rename   
         end else if (fetch_entry_ready_i && issue_n.valid) begin
@@ -56,10 +121,33 @@ module renaming_map import ariane_pkg::*; #(
             issue_q = issue_n;
 
             // TODO: ADD LOGIC TO RENAME OUTGOING INSTRUCTION
-            // The registers of the outgoing instruction issue_q can be set like so:
-            // issue_q.sbe.rs1[PHYS_REG_WIDTH-1:0] = your new rs1 register value;
-            // issue_q.sbe.rs2[PHYS_REG_WIDTH-1:0] = your new rs2 register value;
-            // issue_q.sbe.rd[PHYS_REG_WIDTH-1:0] = your new rd register value;
+            
+            issue_q.sbe.rs1[PHYS_REG_WIDTH-1:0] = rename_table[rs1].valid ? rename_table[rs1].pr : 0;
+            issue_q.sbe.rs2[PHYS_REG_WIDTH-1:0] = rename_table[rs2].valid ? rename_table[rs2].pr : 0;
+            if (rd == 0) begin
+                // Special case: no need to allocate a new physical register.
+                issue_q.sbe.rd[PHYS_REG_WIDTH-1:0] = 0;
+            end else begin
+                // Allocate the lowest-numbered free physical register.
+                assert (~pr_free_map != 0) else $error("No physical registers available!");
+                for (int i = 1; i < PHYS_NUM_REGS; i++) begin
+                    if (pr_free_map[i] == 0) begin
+                        pr_free_map[i] = 1;
+
+                        if (rename_table[rd].valid) begin
+                            // Architectural register currently mapped.
+                            deallocation_map[i].valid = 1;
+                            deallocation_map[i].pr = rename_table[rd].pr;
+                        end
+
+                        rename_table[rd].valid = 1;
+                        rename_table[rd].pr = i;
+
+                        issue_q.sbe.rd[PHYS_REG_WIDTH-1:0] = i;
+                        break;
+                    end
+                end
+            end
     
         // If there is no new instruction this clock cycle, simply pass on the
         // incoming instruction without renaming
@@ -76,7 +164,11 @@ module renaming_map import ariane_pkg::*; #(
             // execute register deallocation logic to reuse physical registers
             if (we_gp_i && waddr_i != 0) begin
         
-                // TODO: IMPLEMENT REGISTER DEALLOCATION LOGIC    
+                // TODO: IMPLEMENT REGISTER DEALLOCATION LOGIC
+                if (deallocation_map[waddr_i].valid) begin
+                    pr_free_map[deallocation_map[waddr_i].pr] = 0;
+                    deallocation_map[waddr_i].valid = 0;
+                end
 
             end
         end
